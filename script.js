52f22e3095f6f0978fe03a65949996c72412abc4
ryanascherr <ryanascherr@gmail.com>
2024-03-31 12:53:31 -0400

    Reworked hand checking and added new functions.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let isInTestingMode = true;
+
 let deckID;
 let isDiscard = false;
 
@@ -5,6 +7,7 @@ const dealBtn = $("#deal-btn");
 const shuffleBtn = $("#shuffle-btn");
 let cards = document.querySelectorAll(".card");
 let handArray = [];
+let arrayOfNumbers = [];
 
 let isFOAK = false;
 let isTOAK = false;
@@ -15,8 +18,9 @@ let isTwoPair = false;
 let isPair = false;
 let currentHand = "";
 
-let handSize = 8;
+let handSize = 52;
 let possibleNumbers = [2,3,4,5,6,7,8,9,10,11,12,13,14];
+let possibleStrings = ['14', '13', '12', '11', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
 let numberOfCardsSelected = 0;
 let enoughCards = true;
 
@@ -37,6 +41,18 @@ $('body').on('click', '.card', function () {
     checkHands();
 });
 
+function makeInitialHandArray() {
+    handArray = [];
+    let cards = document.querySelectorAll(".card");
+    $(selectedHand).each(function( index ) {
+        let currentCard = {
+            'suit': $(this).attr("data-suit"),
+            'number': $(this).attr("data-number")
+        }
+        handArray.push(currentCard);
+    });
+}
+
 $("#discard-btn").click(function() {
     let selectedCards = $(".selected");
     selectedCards.remove();
@@ -52,6 +68,10 @@ function resetHands() {
     isFlush = false;
     isTwoPair = false;
     isPair = false;
+
+    if (isInTestingMode) {
+        console.log("--<> Step 1: All hand types set to false")
+    }
 }
 
 function makeHandArray() {
@@ -64,9 +84,16 @@ function makeHandArray() {
         }
         handArray.push(currentCard);
     });
+
+    console.log("--<> Step 2: Make hand array:");
+    console.log(handArray);
 }
 
 function checkHands() {
+
+    if (isInTestingMode) {
+        console.log("--<> Step 3: Check hand types");
+    }
 
     checkStraightFlush();
     checkFOAK();
@@ -78,125 +105,153 @@ function checkHands() {
     if (isFlush && isStraight) {
         currentHand = "Straight Flush";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a straight flush!");
+        }
     } else if (isFOAK) {
         currentHand = "Four of a Kind";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a four of a kind!");
+        }
     } else if (isFullHouse) {
         currentHand = "Full House";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a full house!");
+        }
     } else if (isFlush) {
         currentHand = "Flush";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a flush!");
+        }
     } else if (isStraight) {
         currentHand = "Straight";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a straight!");
+        }
     } else if (isTOAK) {
         currentHand = "Three of a Kind";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a three of a kind!");
+        }
     } else if (isTwoPair) {
         currentHand = "Two Pair";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a two pair!");
+        }
     } else if (isPair) {
         currentHand = "Pair";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a pair!");
+        }
     } else {
         currentHand = "High Card";
         updateHandStatus(currentHand);
+
+        if (isInTestingMode) {
+            console.log("   --- Is a high card!");
+        }
     }
 }
 
-function updateHandStatus(hand) {
-    $(".hand").text(hand);
-}
-
 function checkStraightFlush() {
+
+    if (isInTestingMode) {
+        console.log(" --- Checking for straight flush")
+    }
+
     checkStraight();
     checkFlush();
 }
 
 function checkFOAK() {
-    let arrayOfNumbers = [];
-    $(handArray).each(function() {
-        let number = this.number;
-        if (number == "ACE") {
-            number = 14;
-        } else if (number == "KING") {
-            number = 13;
-        } else if (number == "QUEEN") {
-            number = 12;
-        } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
-        arrayOfNumbers.push(number);
-    });
 
-
-    arrayOfNumbers.sort(function(a, b) {
-        return a - b;
-    })
-
-    function getOccurrence(array, value) {
-        var count = 0;
-        array.forEach((v) => (v === value && count++));
-        return count;
+    if (isInTestingMode) {
+        console.log(" --- Checking for four of a kind")
     }
 
-    $(possibleNumbers).each(function(number) {
-        repeatNumber = getOccurrence(arrayOfNumbers, number);
-        if (repeatNumber == 4) {
+    convertFaceCards();
+
+    if (isInTestingMode) {
+        if (arrayOfNumbers.length < 4) {
+            console.log("   --- Less than 4 cards - > cannot be a four of a kind");
+        }
+    }
+
+    if (arrayOfNumbers.length < 4) return;
+     
+    const itemCounter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
+
+    $(possibleStrings).each(function() {
+        if(itemCounter(arrayOfNumbers, this) == 4) {
             isFOAK = true;
         }
-    });
+    })
 }
 
 function checkFullHouse() {
-    let arrayOfNumbers = [];
-    $(handArray).each(function() {
-        let number = this.number;
-        if (number == "ACE") {
-            number = 14;
-        } else if (number == "KING") {
-            number = 13;
-        } else if (number == "QUEEN") {
-            number = 12;
-        } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
-        arrayOfNumbers.push(number);
-    });
 
-
-    arrayOfNumbers.sort(function(a, b) {
-        return a - b;
-    })
-
-    if (arrayOfNumbers.length < 5) return;
-
-    function getOccurrence(array, value) {
-        var count = 0;
-        array.forEach((v) => (v === value && count++));
-        return count;
+    if (isInTestingMode) {
+        console.log(" --- Checking for full house")
     }
 
-    let gotTwo = false;
-    let gotThree = false;
-    $(possibleNumbers).each(function(number) {
-        repeatNumber = getOccurrence(arrayOfNumbers, number);
-        if (repeatNumber == 2) {
-            gotThree = true;
-        } else if (repeatNumber == 3) {
-            gotTwo = true;
+    convertFaceCards();
+
+    if (isInTestingMode) {
+        if (arrayOfNumbers.length < 5) {
+            console.log("   --- Less than 5 cards -> cannot be a full house")
         }
-    });
-    if (gotTwo && gotThree) {
+    }
+
+    if (arrayOfNumbers.length < 5) return;
+     
+    const itemCounter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
+
+    let foundAPair = false;
+    let foundATOAK = false;
+    $(possibleStrings).each(function() {
+        if(itemCounter(arrayOfNumbers, this) == 2) {
+            foundAPair = true;
+        }
+        if(itemCounter(arrayOfNumbers, this) == 3) {
+            foundATOAK = true;
+        }
+    })
+
+    if (foundAPair && foundATOAK) {
         isFullHouse = true;
     }
 }
 
 function checkFlush() {
-    if (handArray < 5) return;
+
+    if (isInTestingMode) {
+        console.log(" --- Checking for flush");
+        if (handArray.length < 5) {
+            console.log("   --- Less than 5 cards -> cannot be a flush");
+        }
+    }
+
+    if (handArray.length < 5) return;
+
     let currentSuit = "";
     let number = 0;
     $(handArray).each(function() {
@@ -208,29 +263,37 @@ function checkFlush() {
         } 
         currentSuit = this.suit;
     });
+
+    if (isInTestingMode) {
+        if (isFlush) {
+            console.log("   --- Is a flush!")
+        } else {
+            console.log("   --- Is not a flush")
+        }
+    }
 }
 
 function checkStraight() {
-    let arrayOfNumbers = [];
-    $(handArray).each(function() {
-        let number = this.number;
-        if (number == "ACE") {
-            number = 14;
-        } else if (number == "KING") {
-            number = 13;
-        } else if (number == "QUEEN") {
-            number = 12;
-        } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
-        arrayOfNumbers.push(number);
-    });
 
+    if (isInTestingMode) {
+        console.log(" --- Checking for straight");
+    }
+
+    convertFaceCards();
+
+    var nums = arrayOfNumbers.map(function(str) {
+        return parseInt(str); 
+    });
 
     arrayOfNumbers.sort(function(a, b) {
         return a - b;
     })
+
+    if (isInTestingMode) {
+        if (arrayOfNumbers.length < 5) {
+            console.log("   --- Less than 5 cards -> cannot be a straight")
+        }
+    }
 
     if (arrayOfNumbers.length < 5) return;
 
@@ -242,121 +305,112 @@ function checkStraight() {
         isStraight = true;
     }
 
+    if (isInTestingMode) {
+        if (isStraight) {
+            console.log("   --- Is a straight!")
+        } else {
+            console.log("   --- Is not a straight");
+        }
+    }
+
 }
 
 function checkTOAK() {
-    let arrayOfNumbers = [];
-    $(handArray).each(function() {
-        let number = this.number;
-        if (number == "ACE") {
-            number = 14;
-        } else if (number == "KING") {
-            number = 13;
-        } else if (number == "QUEEN") {
-            number = 12;
-        } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
-        arrayOfNumbers.push(number);
-    });
 
-
-    arrayOfNumbers.sort(function(a, b) {
-        return a - b;
-    })
-
-    function getOccurrence(array, value) {
-        var count = 0;
-        array.forEach((v) => (v === value && count++));
-        return count;
+    if (isInTestingMode) {
+        console.log(" --- Checking for three of a kind")
     }
 
-    $(possibleNumbers).each(function(number) {
-        repeatNumber = getOccurrence(arrayOfNumbers, number);
-        if (repeatNumber == 3) {
+    convertFaceCards();
+
+    if (isInTestingMode) {
+        if (arrayOfNumbers.length < 3) {
+            console.log("   --- Less than 3 cards -> cannot be a three of a kind")
+        }
+    }
+
+    if (arrayOfNumbers.length < 3) return;
+
+     
+    const itemCounter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
+
+    $(possibleStrings).each(function() {
+        if(itemCounter(arrayOfNumbers, this) == 3) {
             isTOAK = true;
         }
-    });
+    })
 }
 
 function checkTwoPair() {
-    let arrayOfNumbers = [];
-    $(handArray).each(function() {
-        let number = this.number;
-        if (number == "ACE") {
-            number = 14;
-        } else if (number == "KING") {
-            number = 13;
-        } else if (number == "QUEEN") {
-            number = 12;
-        } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
-        arrayOfNumbers.push(number);
-    });
 
-
-    arrayOfNumbers.sort(function(a, b) {
-        return a - b;
-    })
-
-    function getOccurrence(array, value) {
-        var count = 0;
-        array.forEach((v) => (v === value && count++));
-        return count;
+    if (isInTestingMode) {
+        console.log(" --- Checking for two pair")
     }
 
-    if (handArray < 4) return;
+    convertFaceCards();
+     
+    const itemCounter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
 
     let numberOfPairs = 0;
-    $(possibleNumbers).each(function(number) {
-        repeatNumber = getOccurrence(arrayOfNumbers, number);
-        if (repeatNumber == 2) {
+    $(possibleStrings).each(function() {
+        if(itemCounter(arrayOfNumbers, this) == 2) {
             numberOfPairs++;
         }
-    });
+    })
 
     if (numberOfPairs == 2) {
         isTwoPair = true;
     }
+    
 }
 
 function checkPair() {
-    let arrayOfNumbers = [];
+
+    if (isInTestingMode) {
+        console.log(" --- Checking for pair");
+    }
+
+    convertFaceCards();
+     
+    const itemCounter = (value, index) => {
+        return value.filter((x) => x == index).length;
+    };
+
+    let numberOfPairs = 0;
+    $(possibleStrings).each(function() {
+        if(itemCounter(arrayOfNumbers, this) == 2) {
+            numberOfPairs++;
+        }
+    })
+
+    if (numberOfPairs == 1) {
+        isPair = true;
+    }
+}
+
+function convertFaceCards() {
+    arrayOfNumbers = [];
     $(handArray).each(function() {
         let number = this.number;
         if (number == "ACE") {
-            number = 14;
+            number = "14";
         } else if (number == "KING") {
-            number = 13;
+            number = "13";
         } else if (number == "QUEEN") {
-            number = 12;
+            number = "12";
         } else if (number == "JACK") {
-            number = 11;
-        }
-        number = parseInt(number);
+            number = "11";
+        } 
         arrayOfNumbers.push(number);
     });
+}
 
-
-    arrayOfNumbers.sort(function(a, b) {
-        return a - b;
-    })
-
-    function getOccurrence(array, value) {
-        var count = 0;
-        array.forEach((v) => (v === value && count++));
-        return count;
-    }
-
-    $(possibleNumbers).each(function(number) {
-        repeatNumber = getOccurrence(arrayOfNumbers, number);
-        if (repeatNumber == 2) {
-            isPair = true;
-        }
-    });
+function updateHandStatus(hand) {
+    $(".hand").text(hand);
 }
 
 function dealCards(){
@@ -369,6 +423,10 @@ function dealCards(){
         numberOfCardsSelected = 0;
     } else {
         howManyCards = handSize;
+    }
+
+    if (isInTestingMode) {
+        console.log("-- Dealing " + howManyCards + " cards")
     }
 
     let dealURL = "https://deckofcardsapi.com/api/deck/" +
@@ -393,9 +451,42 @@ function dealCards(){
                 } else {
                     isDiscard = false;
                 }
+
+                let arrayOfCardObjects = []
+
                 for (let i = 0; i < handSize; i++) {
-                    $(".card-container").append(`<img class="card" src="${data.cards[i].image}" data-suit="${data.cards[i].suit}" data-number="${data.cards[i].value}">`)
+                    let trueValue;
+                    if (data.cards[i].value == "ACE") {
+                        trueValue = 14;
+                    } else if (data.cards[i].value == "JACK") {
+                        trueValue = 11;
+                    } else if (data.cards[i].value == "QUEEN") {
+                        trueValue = 12;
+                    } else if (data.cards[i].value == "KING") {
+                        trueValue = 13;
+                    } else {
+                        trueValue = parseInt(data.cards[i].value);
+                    }
+
+                    let currentObject = {
+                        suit: data.cards[i].suit,
+                        number: trueValue,
+                        image: data.cards[i].image
+                    }
+
+                    arrayOfCardObjects.push(currentObject);
+                    arrayOfCardObjects?.sort((a, b) => (a.number > b.number ? -1 : 1))
                 }
+
+                if (isInTestingMode) {
+                    console.log("Array of cards dealt:");
+                    console.log(arrayOfCardObjects);
+                };
+
+                $.each(arrayOfCardObjects, function(index, card) {
+                    $(".card-container").append(`<img class="card" src="${card.image}" data-suit="${card.suit}" data-number="${card.number}">`)
+                 });
+
             } else {
                 $("#alert-text").text("Not enough cards. Please shuffle deck.");
             }
@@ -414,6 +505,10 @@ function shuffleCards(){
             enoughCards = true;
             $("#alert-text").text("The deck has been shuffled!");
             $(".card-container").empty();
+
+            if (isInTestingMode) {
+                console.log("-- Shuffing cards");
+            }
         })
 }
 
@@ -426,6 +521,11 @@ function initialize(){
                 deckID = data.deck_id;
                 $("#alert-text").text("Cards Remaining: " + data.remaining);
                 shuffleCards();
+
+                if (isInTestingMode) {
+                    console.log("--> Initializing... <--");
+                    console.log("-- Making deck. Deck ID: " + deckID);
+                }
             })
 }
 
